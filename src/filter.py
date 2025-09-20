15248d1c3271daf92a8afeb345f18a4b7101d61a
David Pavlicek <117974146+marshall1405@users.noreply.github.com>
2025-09-20 22:55:26 +0200

    end of day commit

--- src/filter.py
+++ src/filter.py
@@ -1,18 +1,29 @@
-def filter_players(matches, player):
+def filter_players(matches, players):
     output = []
     for m in matches:
-        if player.lower() in m.team1.lower() or player in m.team2.lower():
-            output.append(m)
+        for p in players:
+            if p.lower() in m.team1.lower() or p.lower() in m.team2.lower():
+                output.append(m)
     return output
 
 
-def filter_tournaments(matches, tournament):
+def filter_tournaments(matches, tournaments):
     output = []
     for m in matches:
-        if tournament.lower() in m.league.name.lower():
+        for t in tournaments:
+            if t.lower() in m.league.name.lower():
+                output.append(m)
+    return output
+
+def filter_all(matches, players, tournaments):
+    output = []
+    for m in matches:
+        if any(p.lower() in m.team1.lower().split() or p.lower() in m.team2.lower() for p in players) or any(t.lower() in m.league.name.lower() for t in tournaments):
             output.append(m)
     return output
 
-def filter_all(matches, player, tournament):
-    return
+def create_player_array(players):
+    return players.split(",")
 
+def create_tournament_array(tournaments):
+    return tournaments.split(",")
--- src/main.py
+++ src/main.py
@@ -2,11 +2,17 @@ from fetcher import fetch_html_text
 from html_parser import create_matches_leagues,parse_matches, parse_leagues
 from message_formatter import format_matches_html
 from sender import send_email
-from filter import filter_players, filter_tournaments, filter_all
+from filter import filter_players, filter_tournaments, filter_all, create_player_array, create_tournament_array
 
 
 player_filter = input("Enter the SURNAME of a PLAYER you like: ")
 tournament_filter = input("Enter the name of a TOURNAMENT you like: ")
+
+players = create_player_array(player_filter)
+tournaments = create_tournament_array(tournament_filter)
+
+print(players)
+print(tournaments)
 
 # for now we will use this tennis url
 url = "https://www.livesport.cz/tenis/"
@@ -20,7 +26,7 @@ parse_matches(matches, leagues, html)
 single_future_matches = [m for m in matches if m.time != "No Time" and m.league.single]
 future_matches = [m for m in matches if m.time != "No Time"]
 
-filtered_matches = filter_tournaments(future_matches, tournament_filter)
+filtered_matches = filter_players(future_matches, player_filter)
 
 html = format_matches_html(filtered_matches)
 
