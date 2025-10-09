925cd8790f954a377902d293802f1fcf2ed730b0
David Pavlicek <117974146+marshall1405@users.noreply.github.com>
2025-10-09 20:19:59 +0200

    making gui more tailored

--- src/gui.py
+++ src/gui.py
@@ -1,10 +1,12 @@
 import streamlit as st
+import json 
 
 st.title("🏅 Let's setup your personalized SPORT REMINDER 🏅")
 
-sport = st.text_input("Sport")
+filter = st.text_input("Surnames of your favorite Tennis players")
 email = st.text_input("Email")
-time = st.text_input("Reminder Time")
 
 if st.button("Save Settings"):
-    st.success(f"Reminder set for {sport} at {time}\nEmail: {email}")
+    with open("./reminder_settings/settings.json", "a") as f:
+        json.dump({"filter": filter, "email": email}, f, ensure_ascii=False)
+    st.success("Settings saved!")
